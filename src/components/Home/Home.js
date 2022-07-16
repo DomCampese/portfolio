6e15a7ab0dc084d7eb665d43eab0b101a99f4b5c
Dominic Campese <62118391+DomCampese@users.noreply.github.com>
2022-07-16 16:12:46 -0400

    Updating styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,21 +6,23 @@ import './home-styles.css'
 const Home = () => {
   return (
     <div className='content'>
-      <div className='greeting-wrapper'>
-        <p className='greeting'>Hello.</p>
-      </div>
-      <div className='portrait-wrapper'>
-        <img className='portrait' src={portrait} alt='Dom Campese'></img>
-      </div>
-      <div className='description'>
-        <p>
-          I'm Dom Campese, a Junior at the University of Pittsburgh studying Computer Science. 
-          I'm passionate about Computer Science and full stack web development. Check out some
-          of my work and feel free to contact me!
-        </p>
-      </div>
-      <div className="contact-button-wrapper">
-        <Link className='contact-button' to='/contact'>get in touch</Link>
+      <div className="intro">
+        <div className='portrait-wrapper'>
+          <div>
+            <img className='portrait' src={portrait} alt='Dom Campese'></img>
+          </div>
+        </div>
+        <div className='description'>
+          <p>
+            My name's Dom Campese. I am a Senior at the University of Pittsburgh studying Computer Science
+            with a minor in Spanish. I'm a passionate full-stack Software Engineer with industry experience
+            in React and .NET and am comfortable with multiple other languages, frameworks, and technologies. I live and work in the greator Pittsburgh area. Feel free to check out my work 
+            on <a href='https://www.linkedin.com/in/dominic-campese-560bb31b0/'>Github</a> or <a href='https://www.linkedin.com/in/dominic-campese-560bb31b0/'>LinkedIn</a> and please reach out with any questions or opportunities!
+          </p>
+        </div>
+        <div className="contact-button-wrapper">
+          <Link className='contact-button' to='/contact'>get in touch</Link>
+        </div>
       </div>
     </div>
   )
