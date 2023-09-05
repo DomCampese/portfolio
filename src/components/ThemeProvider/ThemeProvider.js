5dfde5960078218c0b47f7b950d94c704d3aa00d
Dominic Campese <dominic.campese@dcsg.com>
2023-09-05 00:47:17 -0400

    Notch compromise

--- src/components/ThemeProvider/ThemeProvider.js
+++ src/components/ThemeProvider/ThemeProvider.js
@@ -3,9 +3,6 @@ import { useEffect } from 'react'
 const ThemeProvider = ({ isDarkMode }) => {
   useEffect(() => {
     document.documentElement.setAttribute('color-theme', isDarkMode ? 'dark' : 'light');
-    // Ensure the area around the iphone notch matches the background color of the theme
-    const backgroundColorCssVar = getComputedStyle(document.documentElement).getPropertyValue('--background-color');
-    document.getElementById('theme-color').setAttribute('content', backgroundColorCssVar);
   }, [isDarkMode]);
 
   return null;
