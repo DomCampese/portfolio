c72c9171c610fb2f586d02fddb91953eec65ecfa
Dominic Campese <dominic.campese@dcsg.com>
2023-09-04 23:30:41 -0400

    Move notch logic to theme provider

--- src/components/ThemeProvider/ThemeProvider.js
+++ src/components/ThemeProvider/ThemeProvider.js
@@ -3,6 +3,9 @@ import { useEffect } from 'react'
 const ThemeProvider = ({ isDarkMode }) => {
   useEffect(() => {
     document.documentElement.setAttribute('color-theme', isDarkMode ? 'dark' : 'light');
+    // Ensure the area around the iphone notch matches the background color of the theme
+    const backgroundColorCssVar = getComputedStyle(document.documentElement).getPropertyValue('--background-color');
+    document.getElementById('theme-color').setAttribute('content', backgroundColorCssVar);
   }, [isDarkMode]);
 
   return null;
