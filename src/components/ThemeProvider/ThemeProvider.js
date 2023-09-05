ac1d7384363c8844afbea52d4b7faf7b97f2ec86
Dominic Campese <dominic.campese@dcsg.com>
2023-09-04 23:51:02 -0400

    Let safari set theme color automatically

--- src/components/ThemeProvider/ThemeProvider.js
+++ src/components/ThemeProvider/ThemeProvider.js
@@ -3,9 +3,6 @@ import { useEffect } from 'react'
 const ThemeProvider = ({ isDarkMode }) => {
   useEffect(() => {
     document.documentElement.setAttribute('color-theme', isDarkMode ? 'dark' : 'light');
-    // Ensure the area around the iphone notch matches the background color of the theme
-    const backgroundColorCssVar = getComputedStyle(document.documentElement).getPropertyValue('--background-color');
-    document.getElementsByName('theme-color')[0].setAttribute('content', backgroundColorCssVar);
   }, [isDarkMode]);
 
   return null;
