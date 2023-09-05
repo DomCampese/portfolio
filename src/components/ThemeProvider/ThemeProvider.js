6ea5ca27e722f43110f95504d1e9632c9772efec
Dominic Campese <dominic.campese@dcsg.com>
2023-09-04 23:49:11 -0400

    Fix safari notch bug

--- src/components/ThemeProvider/ThemeProvider.js
+++ src/components/ThemeProvider/ThemeProvider.js
@@ -5,7 +5,7 @@ const ThemeProvider = ({ isDarkMode }) => {
     document.documentElement.setAttribute('color-theme', isDarkMode ? 'dark' : 'light');
     // Ensure the area around the iphone notch matches the background color of the theme
     const backgroundColorCssVar = getComputedStyle(document.documentElement).getPropertyValue('--background-color');
-    document.getElementById('theme-color').setAttribute('content', backgroundColorCssVar);
+    document.getElementsByName('theme-color')[0].setAttribute('content', backgroundColorCssVar);
   }, [isDarkMode]);
 
   return null;
