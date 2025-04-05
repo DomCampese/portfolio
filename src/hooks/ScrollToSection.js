83d0b1b06ab4f1eb8d5d1a15adaa9a662d648ec1
Dominic Campese <dominiccampese@Dominics-MacBook-Pro.local>
2025-04-05 19:29:56 -0400

    Remove unecessary validation

--- src/hooks/ScrollToSection.js
+++ src/hooks/ScrollToSection.js
@@ -6,11 +6,6 @@ export const useScrollToSection = () => {
   const isMobile = useMobile();
 
   const scrollToSection = (section) => {
-    if (!Object.values(Section).includes(section)) {
-      console.error(`Tried to scroll to invalid section: ${section}`);
-      return;
-    }
-
     const offset = isMobile ? 10 : 100;
     const element = document.getElementById(section);
     const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
