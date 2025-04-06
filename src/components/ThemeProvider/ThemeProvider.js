e384144adb0890fb011ca6942792770590336cd1
Dominic Campese <dominiccampese@Dominics-MacBook-Pro.local>
2025-04-05 20:07:58 -0400

    Notch color fix

--- src/components/ThemeProvider/ThemeProvider.js
+++ src/components/ThemeProvider/ThemeProvider.js
@@ -1,8 +1,12 @@
 import { useEffect } from 'react'
 
 const ThemeProvider = ({ isDarkMode }) => {
+  const darkGray = '#121212';
+  const white = '#FFFFFF';
+
   useEffect(() => {
     document.documentElement.setAttribute('color-theme', isDarkMode ? 'dark' : 'light');
+    document.querySelector("meta[name='theme-color']").setAttribute("content", isDarkMode ? darkGray : white);
   }, [isDarkMode]);
 
   return null;
